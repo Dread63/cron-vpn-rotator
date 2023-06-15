e6868d2765ab49458ae934517d0ce578773b1ec7
J <123434357+Dread63@users.noreply.github.com>
2023-06-14 23:51:01 -0600

    Add files via upload

---
+++ vpn_rotation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Array of VPN server locations
+servers=("United_States dallas" "United_States miami" "United_States chicago" "United_Kingdom london" "Canada montreal")
+
+# Read the last connected server from a file
+last_server=$(cat /home/dread63/Documents/last_server.txt)
+
+# Remove the last connected server from the array
+servers=("${servers[@]/$last_server}")
+
+# Randomly select a server from the remaining array
+server=${servers[$RANDOM % ${#servers[@]}]}
+
+# Connect to the selected server
+nordvpn connect $server
+
+# Save the selected server as last connected server
+echo "$server" > /home/dread63/Documents/last_server.txt
